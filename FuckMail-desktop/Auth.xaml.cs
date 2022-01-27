0c12a47832ae0df56f12fc47323f15f625e6d76a
amtp1 <magomedov.abdul20012@gmail.com>
2022-01-27 12:06:20 +0300

    Fuckmail-desktop

---
+++ FuckMail-desktop/Auth.xaml.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.Net;
+using System.IO;
+using System.Text.Json;
+using Newtonsoft.Json;
+
+namespace FuckMail_desktop
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class Auth : Window
+    {
+        class SomeType {
+            public bool response { get; set; }
+        }
+
+        public Auth()
+        {
+            if (!Directory.Exists("data"))
+            {
+                Directory.CreateDirectory("data");
+                InitializeComponent();
+            }
+            else {
+                int count_files = Directory.GetFiles("data").Length;
+                if (count_files != 1)
+                {
+                    Directory.Delete("data", true);
+                    Directory.CreateDirectory("data");
+                }
+                else if (count_files == 1){
+
+                    string[] fileName = Directory.GetFiles(@"data");
+                    string data = File.ReadAllText(fileName[0]);
+                    string[] metadata = data.Split(':');
+                    
+                    if (metadata.Length < 2) {
+                        Directory.Delete("data", true);
+                        Directory.CreateDirectory("data");
+                    }
+                    else
+                    {
+                        if (!CheckAuth(metadata[0], metadata[1], false))
+                        {
+                            Directory.Delete("data", true);
+                            Directory.CreateDirectory("data");
+                        }
+                        else
+                        {
+                            string sessionID = fileName[0].Replace(@"data\", "");
+                            Hide();
+                            Main main_window = new Main(metadata[0], sessionID);
+                            main_window.ShowDialog();
+                            Close();
+                        }
+                    }
+                }
+            }
+        }
+
+        private void login_btn_Click(object sender, RoutedEventArgs e)
+        {
+            string username = username_txtBox.Text;
+            string password = password_txtBox.Password;
+
+            if (username == "" || password == "")
+            {
+                MessageBox.Show("Fields don't should be empty!");
+            }
+            else
+            {
+                if (!CheckAuth(username, password, true))
+                {
+                    MessageBox.Show("Incorrect data!");
+                }
+                else
+                {
+                    Random rand = new Random();
+                    var newCongiFileName = CreateMD5(rand.Next(10).ToString());
+
+                    using (FileStream fs = File.Create(String.Format("data/{0}", newCongiFileName)))
+                    {
+                        byte[] w_username = new UTF8Encoding(true).GetBytes(username);
+                        byte[] w_password = new UTF8Encoding(true).GetBytes(CreateMD5(password));
+                        byte[] w_symbol = new UTF8Encoding(true).GetBytes(":");
+                        fs.Write(w_username, 0, w_username.Length);
+                        fs.Write(w_symbol, 0, w_symbol.Length);
+                        fs.Write(w_password, 0, w_password.Length);
+                    }
+
+                    Hide();
+                    Main main_window = new Main(username, newCongiFileName);
+                    main_window.ShowDialog();
+                    Close();
+                }
+            }
+        }
+
+        public bool CheckAuth(string username, string password, bool md5) {
+            string hashPass = md5 ? CreateMD5(password) : password;
+            var url = String.Format("http://127.0.0.1:8000/api/auth/{0}/{1}", username, hashPass);
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+
+            SomeType Response = JsonConvert.DeserializeObject<SomeType>(data);
+            return Response.response;
+        }
+
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString().ToLower();
+            }
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ FuckMail-desktop/Main.xaml.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using MailKit;
+using MailKit.Net.Imap;
+using MailKit.Search;
+using MimeKit;
+
+namespace FuckMail_desktop
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class Main : Window
+    {
+        class SomeType
+        {
+            public string[] addresses { get; set; }
+        }
+
+        class AddressDataType {
+            public _AddressDataType data { get; set; }
+        }
+
+        class _AddressDataType {
+            public string address { get; set; }
+            public string password { get; set; }
+            public string proxy_url { get; set; }
+        }
+
+        public Main(string username, string sessionID)
+        {
+            InitializeComponent();
+            getAllAddresses(username);
+        }
+
+        private void getAllAddresses(string username) {
+            var url = String.Format("http://127.0.0.1:8000/api/addresses/{0}", username);
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+
+            SomeType Addresses = JsonConvert.DeserializeObject<SomeType>(data);
+
+            foreach(string address in Addresses.addresses)
+            {
+                Button address_button = new Button();
+                Label address_label = new Label();
+                address_button.Content = address;
+                address_button.Height = 35;
+                address_button.Name = username;
+                address_button.Click += ClickHandler;
+                addressesPanel.Children.Add(address_button);
+                addressesPanel.Children.Add(address_label);
+            }
+        }
+
+        private void getAllMessages(string username, string address) {
+            var url = String.Format("http://127.0.0.1:8000/api/address_data/{0}/{1}", username, address);
+            var request = WebRequest.Create(url);
+            request.Method = "GET";
+
+            var webResponse = request.GetResponse();
+            var webStream = webResponse.GetResponseStream();
+
+            var reader = new StreamReader(webStream);
+            var data = reader.ReadToEnd();
+
+            AddressDataType address_data = JsonConvert.DeserializeObject<AddressDataType>(data);
+            try
+            {
+                using (var client = new ImapClient())
+                {
+                    client.Connect("imap.outlook.com", 993, true);
+
+                    client.Authenticate(address_data.data.address, address_data.data.password);
+
+                    // The Inbox folder is always available on all IMAP servers...
+                    var inbox = client.Inbox;
+                    inbox.Open(FolderAccess.ReadOnly);
+
+                    for (int i = 0; i < inbox.Count; i++)
+                    {
+                        var message = inbox.GetMessage(i);
+                        Button message_button = new Button();
+                        Label message_label = new Label();
+                        message_button.Height = 40;
+                        message_button.Content = message.Subject;
+                        Console.WriteLine(message.TextBody);
+                        messagesPanel.Children.Add(message_button);
+                        messagesPanel.Children.Add(message_label);
+                    }
+
+                    client.Disconnect(true);
+                }
+            }
+            catch (Exception e) {
+                MessageBox.Show(e.Message);
+            }
+        }
+
+        private delegate void starterdelegate(object sender, RoutedEventArgs e);
+
+        private void ClickHandler(object sender, RoutedEventArgs e)
+        {
+            Button btn = sender as Button;
+            Application.Current.Dispatcher.Invoke(new Action(() => {
+                getAllMessages(btn.Name.ToString(), btn.Content.ToString());
+            }));
+        }
+
+        private void ComboBoxItem_Selected(object sender, RoutedEventArgs e)
+        {
+            Directory.Delete("data", true);
+            this.Hide();
+            Auth auth_window = new Auth();
+            auth_window.ShowDialog();
+            this.Close();
+        }
+
+        public static string CreateMD5(string input)
+        {
+            // Use input string to calculate MD5 hash
+            using (System.Security.Cryptography.MD5 md5 = System.Security.Cryptography.MD5.Create())
+            {
+                byte[] inputBytes = Encoding.ASCII.GetBytes(input);
+                byte[] hashBytes = md5.ComputeHash(inputBytes);
+
+                // Convert the byte array to hexadecimal string
+                StringBuilder sb = new StringBuilder();
+                for (int i = 0; i < hashBytes.Length; i++)
+                {
+                    sb.Append(hashBytes[i].ToString("X2"));
+                }
+                return sb.ToString().ToLower();
+            }
+        }
+    }
+}
